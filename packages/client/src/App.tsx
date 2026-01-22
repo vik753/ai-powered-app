a3a906cd371714292a736dc9695a7cc0cafe3489
IhorKorenets <vik753@gmail.com>
2026-01-22 18:08:27 +0200

    frontend: enhance ChatBot with scrolling, typing indicator, and copy prevention in messages

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,8 @@
 import ChatBot from './components/ChatBot.tsx';
 
 function App() {
-  
-
   return (
-    <div className={'p-4'}>
+    <div className={'p-4 h-screen w-full'}>
       <ChatBot />
     </div>
   );
--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -3,7 +3,8 @@ import ReactMarkdown from 'react-markdown';
 import { Button } from './ui/button';
 import { FaArrowUp } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import * as React from 'react';
 
 type FormData = {
   prompt: string;
@@ -20,10 +21,19 @@ type Message = {
 
 const ChatBot = () => {
   const [messages, setMessages] = useState<Message[]>([]);
+  const [isBotTyping, setIsBotTyping] = useState(false);
+
   const conversationId = useRef(crypto.randomUUID());
+  const lastMessageRef = useRef<HTMLDivElement | null>(null);
+
   const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
+  useEffect(() => {
+    lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
   const onSubmit = async ({ prompt }: FormData) => {
+    setIsBotTyping(true);
     setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
 
     reset();
@@ -33,6 +43,7 @@ const ChatBot = () => {
       conversationId: conversationId.current,
     });
 
+    setIsBotTyping(false);
     setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
   };
 
@@ -43,11 +54,20 @@ const ChatBot = () => {
     }
   };
 
+  const onCopyMessage = (e: React.ClipboardEvent) => {
+    e.preventDefault();
+    const selectedText = window.getSelection()?.toString().trim();
+    if (selectedText) {
+      e.preventDefault();
+      e.clipboardData.setData('text/plain', selectedText);
+    }
+  };
+
   return (
-    <div>
-      <div className={'flex flex-col p-4 gap-y-4 mb-4'}>
+    <div className={'flex flex-col h-full'}>
+      <div className={'flex flex-col p-4 gap-y-4 mb-4 flex-1 overflow-y-auto'}>
         {messages.map((message, index) => (
-          <p
+          <div
             key={index}
             className={`px-3 py-1 rounded-xl max-w-2/3
               ${
@@ -55,10 +75,19 @@ const ChatBot = () => {
                   ? 'self-end bg-blue-600 text-white '
                   : 'self-start bg-gray-100 text-black'
               }`}
+            onCopy={onCopyMessage}
+            ref={index === messages.length - 1 ? lastMessageRef : null}
           >
             <ReactMarkdown>{message.content}</ReactMarkdown>
-          </p>
+          </div>
         ))}
+        {isBotTyping && (
+          <div className={'flex gap-1 px-3 py-1'}>
+            <div className=" w-3 h-3 bg-neutral-800 rounded-xl animate-pulse"></div>
+            <div className="w-3 h-3 bg-neutral-800 rounded-xl animate-pulse [animation-delay:0.2s]"></div>
+            <div className="w-3 h-3 bg-neutral-800 rounded-xl animate-pulse [animation-delay:0.4s]"></div>
+          </div>
+        )}
       </div>
       <form
         className="flex flex-col gap-2 items-end rounded-xl border-2 p-4"
@@ -73,6 +102,7 @@ const ChatBot = () => {
           placeholder="Ask anything"
           maxLength={1000}
           onKeyDown={onKeyDown}
+          autoFocus={true}
         />
         <Button className="rounded-full w-9 h-9" disabled={!formState.isValid}>
           <FaArrowUp />
