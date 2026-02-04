0aec17f127186934071f861eee7175933244d6d8
IhorKorenets <vik753@gmail.com>
2026-02-04 11:44:52 +0200

    frontend: add loading skeleton to ReviewList, improve loading state handling, and update dependencies

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { StarRating } from '@/components/reviews/StarRating.tsx';
+import Skeleton from 'react-loading-skeleton';
 
 type ReviewListProps = {
   productId: number;
@@ -24,11 +25,14 @@ export const ReviewList: React.FC<ReviewListProps> = ({ productId }) => {
     reviews: [],
     summary: null,
   });
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchReviewData = async () => {
+    setIsLoading(true);
     const { data } = await axios.get<Review[]>(
       `/api/products/${productId}/reviews`
     );
+    setIsLoading(false);
     setReviewData({ reviews: data, summary: reviewData?.summary });
   };
 
@@ -36,7 +40,19 @@ export const ReviewList: React.FC<ReviewListProps> = ({ productId }) => {
     fetchReviewData();
   }, []);
 
-  if (!reviewData) return 'Loading...';
+  if (isLoading) {
+    return (
+      <div>
+        {[1, 2, 3].map((i) => (
+          <div key={i} className="flex flex-col gap-y-2 mb-2 border-b pb-2">
+            <Skeleton width={100} />
+            <Skeleton width={180} />
+            <Skeleton count={3} />
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   return (
     <div>
